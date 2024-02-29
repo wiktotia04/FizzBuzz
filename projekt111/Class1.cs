c8f3a4d0d9a7b8b045a7db4eda216be10c79aa5c
Wiktoria Sarnicka <sarnickawiktoria@gmail.com>
2024-02-29 15:59:33 +0100

    Dodaj pliki projektów.

---
+++ projekt111/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projekt111
+{
+    internal class FizzBuzz
+    {
+        private int upper;
+        public FizzBuzz(int upper)
+        {
+            this.upper = upper;
+        }
+    
+
+        public void Display()
+        {
+            for(int i = 1; i < upper; i++)
+            {
+                if (i % 5 == 0 && i % 3 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("FIzz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else 
+                {
+                    Console.WriteLine(i);
+                }
+                
+               
+               
+            }
+        }
+    }
+}
---
+++ projekt111/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace projekt111
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            FizzBuzz fb1 = new FizzBuzz(30);
+            fb1.Display();
+        }
+    }
+}
+
